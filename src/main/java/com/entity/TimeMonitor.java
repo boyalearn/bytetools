dff7b30e2814428068ec19b65738f092bf212e26
zouhuixing <2114517411@qq.com>
2020-05-27 15:17:56 +0800

    Create TimeMonitor.java

---
+++ src/main/java/com/entity/TimeMonitor.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+ * Description: TimeMonitor
+ * Author: zWX827285
+ * Create: 2020/5/21
+ */
+
+package com.bytetools.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zWX827285
+ * @version 1.0.0 2020/5/21
+ * @see
+ * @since PSM 1.0.5
+ */
+public class TimeMonitor {
+    public List<FilterEntity> excludeFilters = new ArrayList<FilterEntity>();
+
+    public List<FilterEntity> includeFilters = new ArrayList<FilterEntity>();
+}
